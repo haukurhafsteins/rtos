7f1a873bd123e73ab34ffe16d8228ded19d72211
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-09-11 15:32:52 +0000

    Fixed bug in remove. Cmd is now fixed on uint32_t

--- include/MsgBus.hpp
+++ include/MsgBus.hpp
@@ -8,85 +8,103 @@
 
 //--------------------------------------------
 // Abstract base for all topics
-class TopicBase {
+class TopicBase
+{
 public:
-    explicit TopicBase(const char* n) : name_(n) {}
+    explicit TopicBase(const char *n) : name_(n) {}
     virtual ~TopicBase() = default;
 
-    const char* getName() const { return name_; }
+    const char *getName() const { return name_; }
     virtual bool notify() = 0;
 
-    void addSubscriber(RtosMsgBuffer& q, uint32_t msgId) {
-        subscribers_.push_back({ &q, msgId });
+    void addSubscriber(RtosMsgBuffer &q, uint32_t msgId)
+    {
+        subscribers_.push_back({&q, msgId});
     }
-    void removeSubscriber(RtosMsgBuffer& q) {
-        subscribers_.remove_if([&](const Sub& s){ return s.q == &q; });
+    void removeSubscriber(RtosMsgBuffer &q)
+    {
+        subscribers_.remove_if([q_ptr = &q](const Sub &s)
+                               { return s.q == q_ptr; });
     }
 
 protected:
-    struct Sub { RtosMsgBuffer* q; uint32_t id; };
+    struct Sub
+    {
+        RtosMsgBuffer *q;
+        uint32_t id;
+    };
     std::list<Sub> subscribers_;
 
 private:
-    const char* name_;
+    const char *name_;
 };
 
 //--------------------------------------------
 // Typed topic: publishes QMsg<Cmd, T>
-template<typename Cmd, typename T>
-class Topic : public TopicBase {
+template <typename PayloadType>
+class Topic : public TopicBase
+{
 public:
-    Topic(const char* name, const T* dataPtr)
+    Topic(const char *name, const PayloadType *dataPtr)
         : TopicBase(name), data_(dataPtr) {}
 
-    const T* getData() const { return data_; }
+    const PayloadType *getData() const { return data_; }
 
-    bool notify() override {
-        if (!data_) return false;
-        for (auto& s : this->subscribers_) {
-            QMsg<Cmd, T> msg(static_cast<Cmd>(s.id), *data_);
+    bool notify() override
+    {
+        if (!data_)
+            return false;
+        for (auto &s : this->subscribers_)
+        {
+            QMsg<uint32_t, PayloadType> msg(static_cast<uint32_t>(s.id), *data_);
             s.q->send(&msg, msg.size());
         }
         return true;
     }
 
 private:
-    const T* data_; // external storage
+    const PayloadType *data_; // external storage
 };
 
 //--------------------------------------------
 // Registry: central message bus
-class MsgBus {
+class MsgBus
+{
 public:
-    template<typename Cmd, typename T>
-    static TopicBase* add(const char* name, const T* data) {
-        if (topics_.find(name) != topics_.end()) return nullptr;
-        auto* t = new Topic<Cmd, T>(name, data);
-        topics_[name] = t;
-        return t;
+    template <typename T>
+    static bool registerTopic(const char *name, Topic<T> *topic)
+    {
+        auto it = topics_.find(name);
+        if (it != topics_.end())
+            return false;
+        topics_[name] = topic;
+        return true;
     }
 
-    static bool remove(const char* name) {
+    static bool remove(const char *name)
+    {
         auto it = topics_.find(name);
-        if (it == topics_.end()) return false;
-        delete it->second;
+        if (it == topics_.end())
+            return false;
         topics_.erase(it);
         return true;
     }
 
-    template<typename C>
-    static bool subscribe(const char* name, RtosMsgBuffer& receiver, C msgId) {
+    static bool subscribe(const char *name, RtosMsgBuffer &receiver, uint32_t msgId)
+    {
         auto it = topics_.find(name);
-        if (it == topics_.end()) return false;
-        it->second->addSubscriber(receiver, static_cast<uint32_t>(msgId));
+        if (it == topics_.end())
+            return false;
+        it->second->addSubscriber(receiver, msgId);
         return true;
     }
 
-    static TopicBase* get(const char* name) {
+    static TopicBase *get(const char *name)
+    {
         auto it = topics_.find(name);
         return (it == topics_.end()) ? nullptr : it->second;
     }
 
 private:
-    inline static std::map<std::string, TopicBase*> topics_{};
+    inline static std::map<std::string, TopicBase *> topics_{};
 };
