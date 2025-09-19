329e5f3f6813e367efc97528b4f6e33a7c2bee7e
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-09-19 10:50:28 +0000

    Moved and/or deleted

--- include/config/Observer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-template <class T>
-struct ConfigObserver
-{
-    virtual ~ConfigObserver() = default;
-    // Return false to veto (keeps old config).
-    virtual bool on_config_apply(const T &newCfg) = 0;
-};
