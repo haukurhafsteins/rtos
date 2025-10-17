2a4c4844c8f2901b7231aba984624c7c8023fa15
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-10-17 16:05:44 +0000

    Added function to get units as an array

--- include/Metrics.hpp
+++ include/Metrics.hpp
@@ -1,15 +1,32 @@
 #pragma once
+#include <array>
 
-enum class Metrics
+struct Metrics
 {
-    none = 0,
-    gram,
-    kg,
-    temperature,
-    humidity,
-    bar,
-    l_per_min,
-    m3,
-    ppm,
-    ug_per_m3,
+    enum class Unit : int32_t
+    {
+        none = 0,
+        temperature,
+        humidity,
+        bar,
+        l_per_min,
+        m3,
+        ppm,
+        ug_per_m3,
+        kg,
+    };
+    static constexpr std::array<Unit, 9> getArray()
+    {
+        return {
+            Unit::none,
+            Unit::temperature,
+            Unit::humidity,
+            Unit::bar,
+            Unit::l_per_min,
+            Unit::m3,
+            Unit::ppm,
+            Unit::ug_per_m3,
+            Unit::kg,
+        };
+    }
 };
