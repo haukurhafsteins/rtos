7094f48667452d7f4abf9ddf703277fa9f982ac4
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-10-24 09:25:08 +0000

    Added mbar, g, tonne and iaq to Metrics

--- include/Metrics.hpp
+++ include/Metrics.hpp
@@ -9,24 +9,32 @@ struct Metrics
         temperature,
         humidity,
         bar,
+        mbar,
         l_per_min,
         m3,
         ppm,
         ug_per_m3,
+        g,
         kg,
+        tonne,
+        iaq,
     };
-    static constexpr std::array<Unit, 9> getArray()
+    static constexpr std::array<Unit, 11> getArray()
     {
         return {
             Unit::none,
             Unit::temperature,
             Unit::humidity,
             Unit::bar,
+            Unit::mbar,
             Unit::l_per_min,
             Unit::m3,
             Unit::ppm,
             Unit::ug_per_m3,
+            Unit::g,
             Unit::kg,
+            Unit::tonne,
+            Unit::iaq,
         };
     }
     static constexpr const char *c_str(Unit metric)
@@ -35,11 +43,15 @@ struct Metrics
             case Unit::temperature: return "temperature";
             case Unit::humidity: return "humidity";
             case Unit::bar: return "bar";
+            case Unit::mbar: return "mbar";
             case Unit::l_per_min: return "l_per_min";
             case Unit::m3: return "m3";
             case Unit::ppm: return "ppm";
             case Unit::ug_per_m3: return "ug_per_m3";
+            case Unit::g: return "g";
             case Unit::kg: return "kg";
+            case Unit::tonne: return "tonne";
+            case Unit::iaq: return "iaq_index";
             default: return "none";
         }
     }
