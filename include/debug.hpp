680055a693f755778b465d42bc10ec31122bcf01
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-11-11 09:17:20 +0000

    Debug is now using Millis.

--- include/debug.hpp
+++ include/debug.hpp
@@ -4,22 +4,22 @@
 #include <esp_timer.h>
 #include "time.hpp"
 
-#define PRINT_FLOAT(name, value, period) \
+#define PRINT_FLOAT_EVERY_MS(name, value, period) \
     {                                    \
-        static float last = 0;           \
-        float now = (float) (1e-6 * rtos::time::now_ms().count()); \
-        if (now - last > period)          \
+        static Millis last = Millis(0);           \
+        Millis now = rtos::time::now_ms(); \
+        if (now - last > Millis(period)) \
         {                                \
             last = now;                  \
             printf(name, value);          \
         }                                \
     }
 
-#define RUN_CODE_EVERY_START(period) \
+#define RUN_CODE_EVERY_MS(period) \
     {                                 \
-        static float last = 0;            \
-        float now = (float) (1e-6 * rtos::time::now_ms().count()); \
-        if (now - last > period)          \
+        static Millis last = Millis(0);  \
+        Millis now = rtos::time::now_ms(); \
+        if (now - last > Millis(period))  \
         {                                 \
             last = now;
 
