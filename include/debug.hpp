5bafffdb1071d6d939951c0aa3bf8f021cffa4c4
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-11-12 14:04:01 +0000

    Added static_assert to check if Millis or not.

--- include/debug.hpp
+++ include/debug.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <type_traits>
 #include <stdio.h>
 #include <esp_timer.h>
 #include "time.hpp"
@@ -16,7 +17,8 @@
     }
 
 #define RUN_CODE_EVERY_Millis(_dbg_period) \
-    {                                 \
+    { \
+        static_assert(std::is_same_v<decltype(_dbg_period), Millis>, "RUN_CODE_EVERY_Millis requires a Millis argument"); \
         static Millis _dbg_last = Millis(0);  \
         Millis _dbg_now = rtos::time::now_ms(); \
         if (_dbg_now - _dbg_last > Millis(_dbg_period))  \
