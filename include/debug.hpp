a9b4716bec8ed3471a80ca9cb4038a25b65c6df4
Haukur Hafsteinsson <haukur.hafsteinsson@marel.com>
2025-11-12 13:08:07 +0000

    Changed name of EVERY_MS to EVERY_Millis to make sure that Millis is needed.

--- include/debug.hpp
+++ include/debug.hpp
@@ -4,22 +4,22 @@
 #include <esp_timer.h>
 #include "time.hpp"
 
-#define PRINT_FLOAT_EVERY_MS(_dbg_name, _dbg_value, _dbg_period) \
+#define PRINT_FLOAT_EVERY_Millis(_dbg_period, _dbg_name, _dbg_value) \
     {                                    \
         static Millis _dbg_last = Millis(0);           \
         Millis _dbg_now = rtos::time::now_ms(); \
-        if (_dbg_now - _dbg_last > Millis(static_cast<long long>(_dbg_period))) \
+        if (_dbg_now - _dbg_last > Millis(_dbg_period)) \
         {                                \
             _dbg_last = _dbg_now;                  \
             printf(_dbg_name, _dbg_value);          \
         }                                \
     }
 
-#define RUN_CODE_EVERY_MS(_dbg_period) \
+#define RUN_CODE_EVERY_Millis(_dbg_period) \
     {                                 \
         static Millis _dbg_last = Millis(0);  \
         Millis _dbg_now = rtos::time::now_ms(); \
-        if (_dbg_now - _dbg_last > Millis(static_cast<long long>(_dbg_period)))  \
+        if (_dbg_now - _dbg_last > Millis(_dbg_period))  \
         {                                 \
             _dbg_last = _dbg_now;
 
